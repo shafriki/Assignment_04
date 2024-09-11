8ba677a685f66234d86ec0d7f0f1e72c14d48e13
shafriki <shafriki.cse@gmail.com>
2024-09-11 15:35:07 +0600

    solved four problem

--- assignmentFour.js
+++ assignmentFour.js
@@ -21,23 +21,21 @@ function calculateTax(income, expenses) {
 // problem -2
 
 
-// function sendNotification(email) {
+function sendNotification(email) {
 
-//     const atRate = email.split("@");
-//     const userName = atRate[0];
-//     const domainName = atRate[1];
+    const atRate = email.split("@");
+    const userName = atRate[0];
+    const domainName = atRate[1];
 
-
-//     if(userName === "" || domainName ==="" || email.indexOf("@")=== -1){
-//         return "Invalid Email";
-//     }
-    
-//     return userName + " sent you an email from "+domainName
-//     }
-//     console.log(sendNotification("shafriki.cse@gmail.com"));
-//     console.log(sendNotification("@gmail.com"));
-//     console.log(sendNotification("shafriki.cse@"));
-//     console.log(sendNotification("shafriki.csegmail.com"));
+    if(userName === "" || domainName ==="" || email.indexOf("@")=== -1){
+        return "Invalid Email";
+    }
+    return userName + " sent you an email from "+domainName;
+}
+    console.log(sendNotification("shafriki.cse@gmail.com"));
+    console.log(sendNotification("@gmail.com"));
+    console.log(sendNotification("shafriki123.cse@gmail.com"));
+    console.log(sendNotification("shafriki.csegmail.com"));
 
 
 // problem 3
@@ -47,17 +45,50 @@ function checkDigitsInName(name) {
         return "Invalid Input"
        }
        else {
-       for(let givenName = 0; givenName < name.length; givenName++) {
+            for(let givenName = 0; givenName < name.length; givenName++) {
+                    if(name[givenName] >= "0" && name[givenName] <= "9") {
+                        return true;
+                    }     
+            } 
+            return false;
+    }
+}
+// console.log(checkDigitsInName("shimul2300"));
+// console.log(checkDigitsInName("shimul"));
+// console.log(checkDigitsInName(["raj"]));
 
-            if(name[givenName] >= "0" && name[givenName] <= "9") {
+// problem 4
+function calculateFinalScore(obj) {
 
-                return true;
-            }     
-     } 
-       return false;
+    let name =obj.name;
+    let testScore =obj.testScore;
+    let schoolGrade =obj.schoolGrade;
+    let isFFamily = obj.isFFamily;
+
+    if (typeof obj !=="object" || typeof name !=="string" || typeof testScore !=="number" || testScore < 0 || testScore > 50 || typeof schoolGrade !=="number" || schoolGrade > 30 || schoolGrade < 0 || typeof isFFamily !=="boolean") {
+        return "Invalid Input";
+    }
+    let totalMarks = (schoolGrade + testScore);
+    
+    if(isFFamily === true) {
+        totalMarks = totalMarks + 20;
+    }
+    let isAdmit = (totalMarks >= 80);
+    return isAdmit;  
+}
+
+// console.log(calculateFinalScore({ name: "Rajib", testScore: 45, schoolGrade: 25, isFFamily: true })); // true
+// console.log(calculateFinalScore({ name: "Rajib", testScore: 45, schoolGrade: 25, isFFamily: false })); // false
+// console.log(calculateFinalScore("hello")); // Invalid Input
+// console.log(calculateFinalScore({ name: "Rajib", testScore: 15, schoolGrade: 25, isFFamily: true })); // false
+
+// problem 5
+function waitingTime(waitingTimes , serialNumber) {
+    // You have to write your code here
+    if(Array.isArray(waitingTime) === false || typeof serialNumber !== "number") {
+        return "Invalid Input";
+    }
     }
 
-}
-    console.log(checkDigitsInName("shimul2300"));
-    console.log(checkDigitsInName("shimul"));
-    console.log(checkDigitsInName(["raj"]));
+     
+    
